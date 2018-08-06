9245cfd7c29252b40b57edc01de8a07ffce3c974
cralor <cralor@163.com>
2018-08-06 10:03:06 +0800

    ccc

---
+++ chap14-oauth2/auth-service/src/main/java/com/cralor/config/WebSecurityConfig.java
@@ -0,0 +1,63 @@
+package com.cralor.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.annotation.Resource;
+
+
+/**
+ * Created by cc.
+ * 2018/8/6 9:13
+ **/
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+    //1、@Autowired和@Qualifier结合使用
+//    @Qualifier("userServiceImpl")
+//    @Autowired
+
+    //2、使用@Resource
+    @Resource
+    UserDetailsService userDetailsService;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+
+        auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
+
+        /* 加密（hash）过程在Test中
+        BCryptPasswordEncoder util = new BCryptPasswordEncoder();
+        for(int i = 0 ; i < 10; i ++){
+            System. out.println(util.encode("admin" ));
+        }*/
+
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                        .anyRequest().authenticated()
+                        .and()
+                        .csrf().disable();//禁用 跨域请求伪造
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+}
